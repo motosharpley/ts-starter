a2ade077ba10fde36419ea5fadcf9e1abafecdcb
Bryan Sharpley <motosharpley@gmail.com>
2018-08-28 13:50:11 -0500

    set up broswerify

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 const gulp = require("gulp");
-const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const tsify = require("tsify");
 
-gulp.task("default", function() {
-  return tsProject.src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest("dist"));
+gulp.task("html", function(){
+  return gulp.src("src/*.html")
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", ["html"], function() {
+  return browserify({
+    basedir: ".",
+    debug: true,
+    entries: ["src/main.ts"],
+    cache: {},
+    packageCache: {}
+  })
+  .plugin(tsify)
+  .bundle()
+  .pipe(source("bundle.js"))
+  .pipe(gulp.dest("dist"));
 });
